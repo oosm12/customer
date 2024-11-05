692a073283227c6457c964981f1d5253e42f8eec
seon min <ksh127979@naver.com>
2024-11-05 16:39:38 +0900

    first commit

---
+++ customer/src/App.js
@@ -0,0 +1,61 @@
+import React,{ Component } from 'react';
+import './App.css';
+import Customer from './component/Customer';
+
+
+const customers = [
+  {
+  'id':1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '961212',
+  'gender':'남자',
+  'job': '대학생'
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '나동빈',
+  'birthday': '961220',
+  'gender':'남자',
+  'job': '대학생'
+},
+{
+  'id':3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '가동빈',
+  'birthday': '961012',
+  'gender':'남자',
+  'job': '대학생'
+}
+]
+
+
+class App extends Component {
+  render() {
+  return (
+    <div>
+      {
+        customers.map(c => {
+          return (
+          <Customer
+
+          key={c.id}
+          id={c.id}
+          image={c.image} 
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+          />
+          );
+        })
+      }
+    </div>
+   
+  ); 
+}
+}
+
+export default App;
+ 
